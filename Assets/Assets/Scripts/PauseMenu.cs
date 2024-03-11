a20353acca974b8e96d374cae85a6f508e48e52e
ouijajuice <eearls@ltu.edu>
2024-03-11 11:55:01 -0400

    general

---
+++ Assets/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused == true)
+        {
+            Time.timeScale = 0f;
+            pauseMenuUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseMenuUI.SetActive(false);
+        }
+    }
+}
